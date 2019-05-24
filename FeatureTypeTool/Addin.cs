3f42bf376c2d0492d5f3e4dc061e7f9649085c55
Barrett Stolzman <bstolzman@doverprecision.com>
2019-05-24 13:59:05 -0500

    Add project files.

---
+++ FeatureTypeTool/Addin.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using  mBox = System.Windows.Forms.MessageBox;
+using FeatureCAM;
+
+namespace FeatureTypeTool
+{
+    public class Addin
+    {
+        static public Application Application { get; set; }
+
+        static public void OnConnect(object obj, tagFMAddInFlags flags)
+        {
+            Application.CommandBars.CreateButton("Utilities", "ListFeatureTypes", tagFMMacroButtonFaceId.eMBFID_Cogs);
+        }
+
+        static public void OnDisConnect(tagFMAddInFlags flags)
+        {
+            Application.CommandBars.DeleteButton("Utilities", "ListFeatureTypes");
+        }
+        static public void ListFeatureTypes()
+        {
+            FMDocument doc = (FMDocument)Application.ActiveDocument;
+            string fList = "";
+            foreach (FMFeature feature in doc.Features)
+            {
+                tagFMFeatureType type;
+                feature.GetFeatureType(out type);
+                fList += $"{feature.Name} {type} \n";
+            }
+            mBox.Show(fList);
+        }
+    }
+}
